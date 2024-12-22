16d49d33ccf441e447bb4c2eeaa24a6c9f43331f
sathappan1989 <a.s.sathappan@hotmail.com>
2024-12-22 07:15:52 -0700

    Basis

---
+++ princeton/src/LearnOneBasis/DataTypesDefinitions.java
@@ -0,0 +1,17 @@
+package LearnOneBasis;
+
+public class DataTypesDefinitions {
+
+	public static void main(String[] args) {
+		
+		int a=12345; 
+		int b=99;
+		int t=a;
+		a=b;
+		b=t;
+		System.out.println(a);
+		System.out.println(b);
+//		System.out.println(t);
+	}
+
+}
