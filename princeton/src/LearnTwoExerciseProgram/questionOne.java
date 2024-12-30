42961c2315317ee1d0d18e8a3c7e5694d4875ed7
sathappan1989 <a.s.sathappan@hotmail.com>
2024-12-30 16:01:29 -0700

    LearnTwoJava

---
+++ princeton/src/LearnTwoExerciseProgram/questionOne.java
@@ -0,0 +1,11 @@
+package LearnTwoExerciseProgram;
+/*Write a program to print numbers from 1 to 10*/
+/*sample output : 1 2 3 4 5 6 7 8 9 10*/
+public class questionOne {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			System.out.print(i + " ");
+		}
+
+	}}
