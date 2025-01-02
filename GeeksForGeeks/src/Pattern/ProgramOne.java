d62e74073d1bfb76cd726d5ecc504f9bf01a7483
sathappan1989 <sathappanas@outlook.com>
2025-01-02 06:59:07 -0700

    patternProgramCommit

--- GeeksForGeeks/src/Pattern/ProgramOne.java
+++ GeeksForGeeks/src/Pattern/ProgramOne.java
@@ -8,6 +8,7 @@ Output:
 * * * * * 
 * */
 public class ProgramOne {
+// ij ij ij ij ij
 // 11 12 13 14 15
 // 21 22 23 24 25
 // 31 32 33 34 35
--- GeeksForGeeks/src/Pattern/ProgramTwo.java
+++ GeeksForGeeks/src/Pattern/ProgramTwo.java
@@ -15,7 +15,40 @@ public class ProgramTwo {
 	// 31 32 33 34 35
 	// 41 42 43 44 45	
 	// 51 52 53 54 55
-		public static void main(String[] args) {
-		
+	
+	//i
+	// 1  1  1  1  1
+	// 2  2  2  2  2
+	// 3  3  3  3  3
+	// 4  4  4  4  4
+	// 5  5  5  5  5
+	
+	//j
+	// 5  4  3  2  1
+	// 5  4  3  2  1
+	// 5  4  3  2  1
+	// 5  4  3  2  1
+	// 5  4  3  2  1
+	
+	// ij
+	// 15  14  13  12  11
+	// 25  24  23  22  21
+	// 35  34  33  32  31
+	// 45  44  43  42  41
+	// 55  54  53  52  51
+	
+			public static void main(String[] args) {
+				for(int i=1;i<=5;i++) {
+					for(int j=5;j>=1;j--) {
+						if(i<j) {
+							System.out.print("  ");
+						}
+						if(i>=j) {
+							System.out.print("*" + " ");
+						}		
+					}
+					System.out.println(" ");
+				}
+	}
+		}
 
-	}}
