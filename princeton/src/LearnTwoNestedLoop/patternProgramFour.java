f5cfb473795e4151944fa690a72a107b1dfc264b
sathappan1989 <sathappanas@outlook.com>
2025-01-01 21:39:05 -0700

    patternProgramFour

--- princeton/src/LearnTwoNestedLoop/patternProgramFour.java
+++ princeton/src/LearnTwoNestedLoop/patternProgramFour.java
@@ -5,10 +5,14 @@ package LearnTwoNestedLoop;
  *1 1 1
  *1 1 1 1*/
 public class patternProgramFour {
-
 	public static void main(String[] args) {
-		
-
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=4;j++) {
+				if(i>=j) {
+					System.out.print(1 + " ");
+				}		
+			}
+			System.out.println(" ");
+		}	
 	}
-
 }
--- princeton/src/LearnTwoNestedLoop/patternProgramThree.java
+++ princeton/src/LearnTwoNestedLoop/patternProgramThree.java
@@ -8,10 +8,12 @@ public class patternProgramThree {
 
 	public static void main(String[] args) {
 		for(int i=1;i<=4;i++) {
-			for(int j=1;j<=4;j++) {
-				//
+			for(int j=i;j>=1;j--) {
+				if(i>=j) {
+					System.out.print(j + " ");
+				}
 			}
-			
+			System.out.println(" ");
 		}
 
 	}
