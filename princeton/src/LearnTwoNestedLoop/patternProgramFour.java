52ea02908c3786c8a52822e951a56aa9c5edab79
sathappan1989 <a.s.sathappan@hotmail.com>
2025-01-01 20:46:58 -0700

    Adding pattern programs

---
+++ princeton/src/LearnTwoNestedLoop/patternProgramFour.java
@@ -0,0 +1,14 @@
+package LearnTwoNestedLoop;
+/*
+ *1
+ *1 1
+ *1 1 1
+ *1 1 1 1*/
+public class patternProgramFour {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- princeton/src/LearnTwoNestedLoop/patternProgramThree.java
+++ princeton/src/LearnTwoNestedLoop/patternProgramThree.java
@@ -1,9 +1,18 @@
 package LearnTwoNestedLoop;
-
+/*1
+ *2 1
+ *3 2 1
+ *4 3 2 1*/
+ 
 public class patternProgramThree {
 
 	public static void main(String[] args) {
-		
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=4;j++) {
+				
+			}
+			
+		}
 
 	}
 
