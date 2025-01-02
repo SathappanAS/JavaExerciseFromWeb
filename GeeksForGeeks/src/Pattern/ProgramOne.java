070d15492e42b87ee9a96fed65547cd05ffa591b
sathappan1989 <sathappanas@outlook.com>
2025-01-01 23:24:47 -0700

    GeeksForGeeksPatternProgarm

---
+++ GeeksForGeeks/src/Pattern/ProgramOne.java
@@ -0,0 +1,28 @@
+package Pattern;
+/*Input : n = 5
+Output: 
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* */
+public class ProgramOne {
+// 11 12 13 14 15
+// 21 22 23 24 25
+// 31 32 33 34 35
+// 41 42 43 44 45	
+// 51 52 53 54 55
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(i>=j) {
+					System.out.print("*" + " ");
+				}		
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
