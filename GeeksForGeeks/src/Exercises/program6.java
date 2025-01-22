505f7891da08a3f14fd5e02eba04a44b41669bcc
sathappan1989 <sathappanas@outlook.com>
2025-01-21 21:48:52 -0700

    oops

--- GeeksForGeeks/src/Exercises/program6.java
+++ GeeksForGeeks/src/Exercises/program6.java
@@ -5,8 +5,43 @@ package Exercises;
 
 /*Output: 5+7i*/
 public class program6 {
+	
+	int real , image;
+	
+	public program6(int real,int image) {
+		this.real=real;
+		this.image=image;
+	}
+	
+	public void showC()
+    {
+        System.out.print(this.real + " +i " + this.image);
+    }
+		
+	public static program6 add(program6 n1, program6 n2) {
+		program6 res = new program6(0, 0);
+		 res.real = n1.real + n2.real;
+		 res.image = n1.image + n2.image;
+		 return res;
+		
+	}
+	
 	public static void main(String[] args) {
 		
-
+		program6 c1 = new program6(4, 5);
+		program6 c2 = new program6(10, 5);
+        
+		System.out.print("first Complex number: ");
+        c1.showC();
+         
+        System.out.print("\nSecond Complex number: ");
+        c2.showC();
+        
+        program6 res = add(c1, c2);
+        System.out.println("\nAddition is :");
+        
+    
+        res.showC();
+        
 	}
 }
--- GeeksForGeeks/src/Pattern/ProgramTwo.java
+++ GeeksForGeeks/src/Pattern/ProgramTwo.java
@@ -50,5 +50,4 @@ public class ProgramTwo {
 					System.out.println(" ");
 				}
 	}
-		}
-
+}
