6d4cc2d854570fb3cc3274df13ccebf42b02b138
sathappan1989 <sathappanas@outlook.com>
2025-01-22 18:15:39 -0700

    W3 program

---
+++ W3Program/src/Constructor/Cat.java
@@ -0,0 +1,14 @@
+package Constructor;
+/*Default Constructor:
+Write a Java program to create a class called 
+"Cat" with instance variables name and age. 
+Implement a default constructor that initializes the name to "Unknown" and the age to 0. Print the values of the variables.*/
+
+public class Cat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
