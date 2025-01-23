917e7351842bd33b69f59172e08af35343002375
sathappan1989 <sathappanas@outlook.com>
2025-01-22 21:59:50 -0700

    DefaultConstructor

--- W3Program/src/Constructor/Cat.java
+++ W3Program/src/Constructor/Cat.java
@@ -5,9 +5,17 @@ Write a Java program to create a class called
 Implement a default constructor that initializes the name to "Unknown" and the age to 0. Print the values of the variables.*/
 
 public class Cat {
-
+	String name;
+	int age;
+	
+	public Cat() {
+		this.name="Unknown";
+		this.age=10;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Cat c1=new Cat();
+		System.out.println(c1.name + " " + c1.age);
 
 	}
 
