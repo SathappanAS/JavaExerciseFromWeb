be453f9c7323b496e2fdb73833dd224b431e480b
sathappan1989 <sathappanas@outlook.com>
2025-01-24 07:06:00 -0700

    Integer Caching

--- GeeksForGeeks/src/ImportantJavaInterviewProgram/TrickyCounter.java
+++ GeeksForGeeks/src/ImportantJavaInterviewProgram/TrickyCounter.java
@@ -3,8 +3,11 @@ package ImportantJavaInterviewProgram;
 public class TrickyCounter {
 
 	public static void main(String[] args) {
-		 Integer i1 = 127;
-		 Integer i2 = 127;
+		
+		//Integer Caching(Wrapper class): -128 to 127 : default --- -128 to 1000
+		
+		 Integer i1 = 999;		//no primitive to primitive : auto parking 
+		 Integer i2 = 999;
 		 System.out.println(i1 == i2);     // Line 1
 		 
 		 Integer i3 = 128;
@@ -15,9 +18,11 @@ public class TrickyCounter {
 		 Integer i6 = new Integer(127);
 		 System.out.println(i5 == i6);     // Line 3
 		 
-		 /*Line 1: Comparing values <=127 so true
+		 	/*Line 1: Comparing values <=127 so true
 			Line 2: values are >127 (-128 to 127 are consider) so false
 			Line 3: comparing objects so both are different instances so false*/
+		 
+		 // -XX:AutoBoxCacheMax=1000
 	}
 
 }
