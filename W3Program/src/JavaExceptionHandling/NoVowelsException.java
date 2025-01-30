415c9232a37890b2f9e981749406ec2e1cd4faee
sathappan1989 <sathappanas@outlook.com>
2025-01-29 18:23:11 -0700

    NoVowels

---
+++ W3Program/src/JavaExceptionHandling/NoVowelsException.java
@@ -0,0 +1,9 @@
+package JavaExceptionHandling;
+
+public class NoVowelsException extends Exception {
+	 
+	public NoVowelsException(String message) {
+		    super(message);
+	}
+
+}
---
+++ W3Program/src/JavaExceptionHandling/Vowel_Check.java
@@ -0,0 +1,35 @@
+package JavaExceptionHandling;
+/*Write a Java program to create a method that takes a string as input 
+ * and throws an exception if the string does not contain vowels.*/
+public class Vowel_Check {
+
+	public static void checkVowels(String text) throws NoVowelsException{
+		boolean containsVowels = false;
+	    String vowels = "aeiouAEIOU";
+	    
+	    for(int i=0;i<text.length();i++) {
+	    	char ch=text.charAt(i);
+	    	 if (vowels.contains(String.valueOf(ch))) {
+	    	        containsVowels = true;
+	    	        break;
+	    	      }
+	    	    }
+	    	    if (!containsVowels) {
+	    	      throw new NoVowelsException("String does not contain any vowels.");
+	    	    }
+	    	  }
+	   
+	public static void main(String[] args) {
+	
+		   try {
+			      String text = "Java handling and managing exceptions ";
+			      // String text = "Typy gyps fly.";
+			      System.out.println("Original string: " + text);
+			      checkVowels(text);
+			      System.out.println("String contains vowels.");
+			    } catch (NoVowelsException e) {
+			      System.out.println("Error: " + e.getMessage());
+			    }
+	}
+
+}
