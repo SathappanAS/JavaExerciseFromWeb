bd81dc8059c302bebda20b41157b605f9e091543
sathappan1989 <sathappanas@outlook.com>
2025-01-29 18:00:49 -0700

    FileNotFound

---
+++ W3Program/src/JavaExceptionHandling/File_Read.java
@@ -0,0 +1,26 @@
+package JavaExceptionHandling;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/*Write a Java program to create a method that reads a file and throws an exception if the file is not found.*/
+public class File_Read {
+
+	public static void main(String[] args) {
+		
+		File file = new File("c:\\docs\\test.xlsx");
+		
+		
+		try {
+			FileInputStream IP = new FileInputStream(file);
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Program closed");
+
+	}
+
+}
